150cab5dde420e753256dcebfce71c6f8b4023ad
Beuche <76558020+Beuche@users.noreply.github.com>
2020-12-24 14:18:33 +0100

    Aktualisiere main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+while (true) {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        basic.showString("0")
+    }
+    
+    if (input.pinIsPressed(TouchPin.P1)) {
+        basic.showString("1")
+    }
+    
+    if (!input.pinIsPressed(TouchPin.P0)) {
+        basic.clearScreen()
+    }
+    
+    if (!input.pinIsPressed(TouchPin.P0)) {
+        basic.clearScreen()
+    }
+    
+}
