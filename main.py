81882eae1b37eb1de586c52e1d67a340e94b4889
Beuche <76558020+Beuche@users.noreply.github.com>
2021-02-15 13:54:21 +0100

    Aktualisiere pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 while True:
     if input.pin_is_pressed(TouchPin.P0):
         basic.show_string("0")
+    if not input.pin_is_pressed(TouchPin.P0):
+        basic.clear_screen()
     if input.pin_is_pressed(TouchPin.P1):
         basic.show_string("1")
-    if not input.pin_is_pressed(TouchPin.P0) and not input.pin_is_pressed(TouchPin.P1):
+    if not input.pin_is_pressed(TouchPin.P1):
         basic.clear_screen()
+
+
+
+
+        
--- main.ts
+++ main.ts
@@ -3,11 +3,15 @@ while (true) {
         basic.showString("0")
     }
     
+    if (!input.pinIsPressed(TouchPin.P0)) {
+        basic.clearScreen()
+    }
+    
     if (input.pinIsPressed(TouchPin.P1)) {
         basic.showString("1")
     }
     
-    if (!input.pinIsPressed(TouchPin.P0) && !input.pinIsPressed(TouchPin.P1)) {
+    if (!input.pinIsPressed(TouchPin.P1)) {
         basic.clearScreen()
     }
     
